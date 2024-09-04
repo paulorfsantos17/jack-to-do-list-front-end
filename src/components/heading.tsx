18e0987ff262b39c6d323f5862e5b8ea9e38180c
paulorfsantos17 <pauorfsantos17@gmail.com>
2024-09-04 20:43:35 -0300

    styles: add responsive heading

--- src/components/heading.tsx
+++ src/components/heading.tsx
@@ -6,8 +6,13 @@ interface HeadingProps {
 export function Heading({ title, subTitle }: HeadingProps) {
   return (
     <div className="flex flex-col items-center gap-4">
-      <h1 className="font-bold text-4xl text-gray-700">{title}</h1>
-      <p className="text-gray-700 text-center text-md w-4/6">
+      <h1 className="font-bold text-4xl text-gray-700
+        lg:text-white lg:text-7xl"
+      >{title}
+      </h1>
+      <p className="text-gray-700 text-center text-md w-4/6
+      lg:text-white lg:text-4xl lg:w-full lg:leading-relaxed"
+      >
         {subTitle}
       </p>
     </div>
