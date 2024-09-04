573928881290865f949b6c569432fe69d78474ed
paulorfsantos17 <pauorfsantos17@gmail.com>
2024-09-04 16:08:22 -0300

    chore: config eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,14 @@
 import config from '@rocketseat/eslint-config/react.mjs'
+import simpleImportSort from 'eslint-plugin-simple-import-sort'
 
-export default config
+export default [
+  ...config,
+  {
+    plugins: {
+      'simple-import-sort': simpleImportSort,
+    },
+    rules: {
+      'simple-import-sort/imports': 'error',
+    },
+  },
+]
