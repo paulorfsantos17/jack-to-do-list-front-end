ac1a97c1313391dced46a2e656fe430a60b56f3e
paulorfsantos17 <pauorfsantos17@gmail.com>
2024-09-04 16:34:50 -0300

    feat: add input

---
+++ src/components/input.tsx
@@ -0,0 +1,36 @@
+import {
+  forwardRef,
+  InputHTMLAttributes,
+  LegacyRef,
+} from 'react'
+
+type InputProps = InputHTMLAttributes<HTMLInputElement> & {
+  title: string
+
+}
+
+export const Input = forwardRef(({
+  title,
+  id,
+  ...rest
+}: InputProps,
+ref: LegacyRef<HTMLInputElement>) => {
+  return (
+    <div className="flex flex-col gap-2">
+      <label
+        htmlFor={id}
+        className="text-2xl text-gray-700"
+      >
+        {title}
+      </label>
+      <input
+        ref={ref}
+        {...rest}
+        id={id}
+        className="p-3 rounded-lg bg-gray-300 text-gray-600
+          placeholder:text-md  placeholder:text-gray-700"
+      />
+
+    </div>
+  )
+})
