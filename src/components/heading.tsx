fbde2a57eef79d0ff1c80d1dd2617a7b01965b7e
paulorfsantos17 <pauorfsantos17@gmail.com>
2024-09-04 20:58:32 -0300

    styles: refactor font-size and line heading and gap

--- src/components/heading.tsx
+++ src/components/heading.tsx
@@ -5,13 +5,13 @@ interface HeadingProps {
 
 export function Heading({ title, subTitle }: HeadingProps) {
   return (
-    <div className="flex flex-col items-center gap-4">
+    <div className="flex flex-col items-center gap-14">
       <h1 className="font-bold text-4xl text-gray-700
         lg:text-white lg:text-7xl"
       >{title}
       </h1>
       <p className="text-gray-700 text-center text-md w-4/6
-      lg:text-white lg:text-4xl lg:w-full lg:leading-relaxed"
+      lg:text-white lg:text-2xl lg:w-full lg:leading-snug"
       >
         {subTitle}
       </p>
