0a53de6f148a7543a6fe14150a1f6970ab6c6aa4
paulorfsantos17 <pauorfsantos17@gmail.com>
2024-09-04 20:46:32 -0300

    feat: add page login

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,48 +1,58 @@
+import { Link } from 'react-router-dom'
+
 import { Button } from '../components/button'
 import { Heading } from '../components/heading'
 import { Input } from '../components/input'
 
 export function Login() {
   return (
-    <div className="flex flex-col  px-8 py-20 ">
-      <Heading
-        title="Login"
-        subTitle="Organize suas tarefas e alcance
-          seus objetivos com facilidade."
-      />
-
-      <form className="w-full flex flex-col gap-14 mt-20">
-        <Input
-          title="E-mail:"
-          id="email"
-          placeholder="Digite seu e-mail"
-        />
-        <Input
-          title="Senha:"
-          id="password"
-          placeholder="Digite sua senha"
-        />
-
-        <div className="w-full flex justify-center">
-          <Button.Root typeStyle="info">
-            <Button.Title title="Entrar" />
-          </Button.Root>
-        </div>
-      </form>
-
-      <p
-        className="text-gray-700 text-center leading-tight mt-44"
+    <div className="flex flex-col  px-8 py-20
+      lg:flex-row lg:bg-gray-400 lg:px-0 lg:py-0 lg:justify-end"
+    >
+      <div className="lg:w-full lg:flex lg:h-screen
+        lg:justify-center lg:items-center"
       >
-        Não tem uma conta?
-        <a
-          href="#"
-          className="text-gray-400"
+        <Heading
+          title="Login"
+          subTitle="Organize suas tarefas e alcance
+          seus objetivos com facilidade."
+        />
+      </div>
+
+      <div className="lg:bg-white  lg:w-6/12 lg:px-14 lg:pt-8">
+        <form className="w-full flex flex-col gap-14 mt-20">
+          <Input
+            title="E-mail:"
+            id="email"
+            placeholder="Digite seu e-mail"
+          />
+          <Input
+            title="Senha:"
+            id="password"
+            placeholder="Digite sua senha"
+          />
+
+          <div className="w-full flex justify-center">
+            <Button.Root typeStyle="info">
+              <Button.Title title="Entrar" />
+            </Button.Root>
+          </div>
+        </form>
+
+        <p
+          className="text-gray-700 text-center leading-tight mt-24"
         >
+          Não tem uma conta?
+          <Link
+            to="/auth/register"
+            className="text-gray-400"
+          >
           &nbsp;Crie a sua agora
-        </a>
-        &nbsp; e comece a organizar suas tarefas de forma eficiente.
-        Cadastre-se
-      </p>
+          </Link>
+          &nbsp; e comece a organizar suas tarefas de forma eficiente.
+          Cadastre-se
+        </p>
+      </div>
 
     </div>
 
