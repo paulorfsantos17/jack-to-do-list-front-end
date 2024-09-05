fdb97090a89dd6462923b44253517368d554189c
paulorfsantos17 <pauorfsantos17@gmail.com>
2024-09-05 16:23:40 -0300

    feat: add error message

--- src/components/input.tsx
+++ src/components/input.tsx
@@ -7,6 +7,7 @@ import {
 type InputProps = InputHTMLAttributes<HTMLInputElement> & {
   title: string
   id: string
+  error?: string
 }
 
 export const Input = forwardRef(({
@@ -26,11 +27,15 @@ ref: Ref<HTMLInputElement>) => {
       <input
         ref={ref}
         id={id}
+        name={id}
         className="p-3 rounded-lg bg-gray-300 text-gray-600
           placeholder:text-md  placeholder:text-gray-700
           focus:outline-none focus:ring-gray-600 focus:ring-2"
         {...rest}
       />
+      {rest.error && (
+        <p className="text-danger text-xs">{rest.error}</p>
+      )}
 
     </div>
   )
