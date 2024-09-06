e65a009b742360e8c78ddac139e07fe5d641233e
paulorfsantos17 <pauorfsantos17@gmail.com>
2024-09-06 10:01:48 -0300

    feat: ensure token is sent using cookie

--- src/libs/axios.ts
+++ src/libs/axios.ts
@@ -1,8 +1,28 @@
 import axios from 'axios'
+import { Cookies } from 'react-cookie'
 
-export const api = axios.create({
+const api = axios.create({
   baseURL: 'http://localhost:3333',
   headers: {
     'Content-Type': 'application/json',
   },
 })
+
+const cookies = new Cookies()
+
+api.interceptors.request.use(
+  config => {
+    const token = cookies.get('accessToken')
+
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`
+    }
+
+    return config
+  },
+  error => {
+    return Promise.reject(error)
+  },
+)
+
+export { api }
