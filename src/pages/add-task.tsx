48eef92c1b0e098cb3cebc8748b6c0fb74a7c883
paulorfsantos17 <pauorfsantos17@gmail.com>
2024-09-06 14:19:57 -0300

    feat:ensure what is possible add a task

--- src/pages/add-task.tsx
+++ src/pages/add-task.tsx
@@ -1,13 +1,40 @@
 import { Plus, X } from '@phosphor-icons/react'
+import { AxiosError } from 'axios'
+import { type ChangeEvent, type MouseEvent, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
+import { z } from 'zod'
 
 import { Button } from '../components/button'
 import { HeadingTitle } from '../components/heading-title'
 import { Input } from '../components/input'
 import { TextArea } from '../components/text-area'
+import { useTasksContext } from '../hooks/useTasksContext'
 import useWindowSize from '../hooks/useWindowSize'
+import { validateForms } from '../utils/validation'
+
+const addTaskFormSchema = z.object({
+  title: z.string().min(5, 'Título tem que ter no mínimo 5 caracteres.'),
+  description: z.string()
+    .min(3, 'Description precisa conter no mínimo 3 caracteres')
+    .max(100, 'Description precisa conter no máximo 100 caracteres'),
+})
+
+type AddTaskFormSchema = z.infer<typeof addTaskFormSchema>
 
 export function AddTask() {
+  const { addTask } = useTasksContext()
+
+  const [formData, setFormData] = useState<AddTaskFormSchema>({
+    title: '',
+    description: '',
+  })
+
+  const [errorsValidation, setErrorsValidation] =
+  useState<Partial<AddTaskFormSchema >>({})
+
+  const [errorsRequest, setErrorRequest] =
+  useState<string>('')
+
   const navigate = useNavigate()
   const { width } = useWindowSize()
 
@@ -15,6 +42,41 @@ export function AddTask() {
 
   function handleNavigateManagerTask() {
     navigate('/')
+  }
+
+  function handleInputChange(event: ChangeEvent<
+      HTMLInputElement |
+      HTMLTextAreaElement
+    >,
+  ) {
+    const { name, value } = event.target
+    setFormData({
+      ...formData,
+      [name]: value,
+    })
+  }
+
+  async function handleSubmit(event: MouseEvent<HTMLButtonElement>) {
+    event.preventDefault()
+    const { isValid, errors } = validateForms({
+      schema: addTaskFormSchema,
+      data: formData,
+    })
+    if (!isValid) {
+      return setErrorsValidation(errors as Partial<AddTaskFormSchema>)
+    } else {
+      setErrorsValidation({})
+    }
+
+    try {
+      await addTask(formData)
+      navigate('/')
+    } catch (error) {
+      if (error instanceof AxiosError && error.status === 401) {
+        return setErrorRequest('Email ou Senha está errado.')
+      }
+      setErrorRequest('Ocorreu um erro ao tentar fazer o login.')
+    }
   }
 
   return (
@@ -47,21 +109,33 @@ export function AddTask() {
             id="title"
             title="Titulo:"
             placeholder="Digite o titulo da tarefa"
+            onChange={handleInputChange}
+            error={errorsValidation.title}
           />
           <TextArea
             id="description"
             title="Descrição:"
             placeholder="Digite a descrição da tarefa"
+            onChange={handleInputChange}
+            error={errorsValidation.description}
           />
 
           <div className="w-full flex justify-end gap-6">
+
+            {errorsRequest && (
+              <p className="text-danger text-md text-center">{errorsRequest}</p>
+            )}
+
             <Button.Root
               variant="danger"
               onClick={handleNavigateManagerTask}
             >
               <Button.Title title="Cancelar" />
             </Button.Root>
-            <Button.Root variant="success">
+            <Button.Root
+              variant="success"
+              onClick={handleSubmit}
+            >
               <Button.Title title="Adicionar" />
               {!isScreenMd && <Plus fontSize={30} className="text-white" />}
             </Button.Root>
