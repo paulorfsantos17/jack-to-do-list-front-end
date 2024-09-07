5758465e883b07857e6b7b9ac3b95019b662f195
paulorfsantos17 <pauorfsantos17@gmail.com>
2024-09-07 14:23:40 -0300

    feat: add ensure getTask

--- src/pages/task.tsx
+++ src/pages/task.tsx
@@ -1,21 +1,47 @@
+import { useCallback, useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+
 import { HeadingTitle } from '../components/heading-title'
 import { TextArea } from '../components/text-area'
+import type { TaskDto } from '../dto/task-dto'
+import { useTasksContext } from '../hooks/useTasksContext'
 
 export function Task() {
-  return (
+  const { tasksList } = useTasksContext()
+  const [task, setTask] = useState<TaskDto>()
+  const { id } = useParams<{ id: string }>()
 
+  const getTask = useCallback(() => {
+    const task = tasksList.find((task) => task.id === id) || {} as TaskDto
+
+    if (!task) {
+      return null
+    }
+
+    setTask(task)
+  }, [tasksList, id])
+
+  useEffect(() => {
+    getTask()
+  }, [getTask])
+  if (!task) {
+    return null // Return early if task is not found.
+  }
+
+  return (
     <div className="w-full flex flex-col items-center  px-6 gap-8
         lg:bg-white sm:w-9/12 lg:rounded-lg lg:h-4/5
         "
     >
 
-      <HeadingTitle title="Titulo da task" size="small" />
+      <HeadingTitle title={task?.title} size="small" />
 
       <TextArea
         id="description"
         title="Descrição:"
         placeholder="Digite a descrição da tarefa"
         disabled
+        value={task?.description}
       />
 
     </div>
