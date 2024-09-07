e842eb2fa39eb967d8e6aa257ec59b4138b3f9ba
paulorfsantos17 <pauorfsantos17@gmail.com>
2024-09-07 16:05:24 -0300

    test: add heading

---
+++ src/components/heading.spec.tsx
@@ -0,0 +1,19 @@
+import { render, screen } from '@testing-library/react'
+import { describe, expect, it } from 'vitest'
+
+import { Heading } from './heading'
+
+describe('Heading Title Component', () => {
+  it('renders HeadingTitle value size default and class default', () => {
+    render(<Heading title="Title" subTitle="description" />)
+    const title = screen.getByText('Title')
+    const subTitle = screen.getByText('description')
+    expect(title).toBeInTheDocument()
+    expect(subTitle).toBeInTheDocument()
+
+    expect(title)
+      .toHaveClass('font-bold text-4xl text-gray-700')
+    expect(subTitle)
+      .toHaveClass('text-gray-700 text-center text-md')
+  })
+})
