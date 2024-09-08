07f413735d3a1abd3d03cd4d54ee7cda6ea85b12
paulorfsantos17 <pauorfsantos17@gmail.com>
2024-09-08 17:52:24 -0300

    test: add auth e2e

--- src/http/mocks/handlers.ts
+++ src/http/mocks/handlers.ts
@@ -1,5 +1,7 @@
+import { authUserMock } from './auth-user-mock'
 import { registerRestaurantMock } from './register-user-mock'
 
 export const handlers = [
   registerRestaurantMock,
+  authUserMock,
 ]
---
+++ tests/auth.e2e-spec.ts
@@ -0,0 +1,24 @@
+import { expect, test } from '@playwright/test'
+
+test('authenticated successfully', async ({ page }) => {
+  await page.goto('/auth', { waitUntil: 'networkidle' })
+
+  await page.getByLabel('E-mail').fill('paulo@example.com')
+
+  await page.getByLabel('Senha').fill('password')
+
+  await page.getByRole('button', { name: 'Entrar' }).click()
+
+  expect(page.url()).toContain('/')
+})
+test('authenticated Unauthorized', async ({ page }) => {
+  await page.goto('/auth', { waitUntil: 'networkidle' })
+
+  await page.getByLabel('E-mail').fill('paulo@example.com')
+
+  await page.getByLabel('Senha').fill('passwordError')
+
+  await page.getByRole('button', { name: 'Entrar' }).click()
+
+  await expect(page.getByText('Email ou Senha está errado.')).toBeVisible()
+})
