c1439e0604706cc9dc0e0f5a7b224414a6a3dd30
paulorfsantos17 <pauorfsantos17@gmail.com>
2024-09-08 19:00:21 -0300

    refactor: remove import no used

--- tests/manager-task.e2e-spec.ts
+++ tests/manager-task.e2e-spec.ts
@@ -1,4 +1,4 @@
-import { expect, locator, test } from '@playwright/test'
+import { expect, test } from '@playwright/test'
 
 // usa o estado do login antes de iniciar o test
 test.use({ storageState: 'storageState.json' })
